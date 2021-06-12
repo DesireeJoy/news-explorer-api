cff575af13120ae5bdf1e3d280fa92bb5f844baa
DesireeJoy <desireebradish@gmail.com>
2021-06-12 17:41:18 -0400

    Add mongoose

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 // listen to port 3000
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+//connect to mongodb servwe
+mongoose.connect('mongodb://localhost:27017/finaldb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
 
 app.listen(PORT, () => {
   // if everything works fine, the console will show which port the application is listening to
