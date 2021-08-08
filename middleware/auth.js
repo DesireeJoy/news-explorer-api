4ac90cda4f9edbbf29f4b2a24c0f44b09d14135a
DesireeJoy <desireebradish@gmail.com>
2021-08-07 21:29:54 -0400

    Check but ready to delete

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,28 +1,31 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+
 const { AuthError } = require('./errorhandling');
 
 const { NODE_ENV = 'production', JWT_SECRET = 'dev-secret' } = process.env;
 
 function auth(req, res, next) {
   const { authorization } = req.headers;
+  console.log(req.headers);
   if (!authorization) {
-    console.log(authorization);
-    throw new AuthError('Authorization required!a');
+    throw new AuthError('Authorization required!');
   }
   if (!authorization.startsWith('Bearer ')) {
-    throw new AuthError('Authorization required!b!');
+    throw new AuthError('Authorization required!!');
   }
+  console.log(authorization);
   const token = authorization.replace('Bearer ', '');
   console.log(token);
   let payload;
+
   try {
     payload = jwt.verify(
       token,
-      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+      JWT_SECRET : 'dev-secret',
     );
   } catch (err) {
-    throw new AuthError('Authorization requiredc.');
+    throw new AuthError('Authorization required.');
   }
 
   req.user = payload;
