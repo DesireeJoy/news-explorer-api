03aa97191ac9ba44b131013f3820f3c028d8691c
DesireeJoy <desireebradish@gmail.com>
2021-10-20 19:49:13 -0400

    Add name to celebrate validation index in routes2

--- routes/index.js
+++ routes/index.js
@@ -30,7 +30,7 @@ router.post(
       .keys({
         email: Joi.string().required().email(),
         password: Joi.string().required().min(8),
-        name: Joi.string().required.min(2).max(30),
+        name: Joi.string().required().min(2).max(30),
       })
       .unknown(true),
   }),
